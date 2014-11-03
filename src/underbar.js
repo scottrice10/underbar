bd97a8622535c9c2ac20f908468897ed8f36cd0e
Scott Rice <scottrice10@gmail.com>
2014-11-02 22:12:32 -0600

    Fixed _.map tests.

--- src/underbar.js
+++ src/underbar.js
@@ -129,6 +129,12 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(item){
+      result.push(iterator(item));
+    });
+
+    return result;
   };
 
   /*
