18b38205b642d5b12fadbf2d7f1c8e4dbb949be2
Scott Rice <scottrice10@gmail.com>
2014-11-02 22:11:55 -0600

    Fixed _.reject tests.

--- src/underbar.js
+++ src/underbar.js
@@ -96,6 +96,16 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var acceptedCollection = _.filter(collection, test);
+    var rejectedCollection = [];
+
+    _.each(collection, function(item) {
+      if(_.indexOf(acceptedCollection, item) === -1){
+        rejectedCollection.push(item);
+      }
+    });
+
+    return rejectedCollection;
   };
 
   // Produce a duplicate-free version of the array.
