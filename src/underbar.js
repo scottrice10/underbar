884c97d22ad91d7673a4ff464c6c0d24b93d370f
Scott Rice <scottrice10@gmail.com>
2014-11-02 22:12:42 -0600

    Fixed _.invoke tests.

--- src/underbar.js
+++ src/underbar.js
@@ -158,6 +158,11 @@ var _ = {};
   // Calls the method named by functionOrKey on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    var isFunc = (typeof(functionOrKey) === typeof Function);
+
+    return _.map(collection, function(value) {
+      return (isFunc ? functionOrKey : value[functionOrKey]).apply(value, args);
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
